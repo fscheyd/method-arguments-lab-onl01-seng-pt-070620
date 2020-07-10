f6545d106a5caff507d9fbe27440a5ef9b4c676e
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-07-10 01:50:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 #lib/introduction.RUBY_VERSION
 
 def introduction(name, ember.js)
-  puts "Hi, my name is #{name}, and i am learning to program with #{ember.js}."
+  puts "Hi, my name is #{name}."
 end
